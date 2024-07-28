bffd2321d97cc8866e5e0f4f7697a7adf8ac13d2
Bhavishya Tomer <bhavishyatomer@gmail.com>
2024-07-28 11:55:16 +0530

    first commit

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CustomNav from './components/Custom-Nav';
+
+const App = () => {
+  return (
+    <div className='App'>
+    <CustomNav/>
+    </div>
+  )
+}
+
+export default App
